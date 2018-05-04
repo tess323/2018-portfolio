d120687203dd4e3e4a7cb1ba4f34944029f5a3c4
Tess <tess@fuseiq.com>
2018-05-04 16:21:49 -0700

    starting images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('work bitch');
+
+function updateProgress(num1, num2){
+ var percent = Math.ceil( num1 / num2 * 100 ) + '%';
+ console.log ('hello');
+ document.getElementById('progress').style.width = percent;
+}
+
+window.addEventListener('scroll', function(){
+ var top = window.scrollY;
+ var height = document.body.getBoundingClientRect().height - window.innerHeight;
+ updateProgress(top, height);
+});
